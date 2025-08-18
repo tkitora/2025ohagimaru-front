b43634cf91af307caefdb85b3858d8c5deb69ded
yamanekt <yamakei0929neta@gmail.com>
2025-08-19 04:17:42 +0900

    コメント追加

--- src/components/MovingCloud.tsx
+++ src/components/MovingCloud.tsx
@@ -4,7 +4,7 @@ import cloud2Image from '../assets/Home/cloud2.png';
 import cloud3Image from '../assets/Home/cloud3.png';
 import cloud4Image from '../assets/Home/cloud4.png';
 
-const clouds = [
+const clouds = [//雲の上からのなん％か、速度、最初に配置される位置を調整
   { src: cloud1Image, top: '5%', duration: '60s', delay: '-10s' },
   { src: cloud2Image, top: '2%', duration: '60s', delay: '-40s' },
   { src: cloud3Image, top: '20%', duration: '60s', delay: '-30s' },
@@ -31,7 +31,7 @@ const MovingClouds = () => {
           key={index}
           src={cloud.src}
           alt={`Cloud ${index + 1}`}
-          className="absolute z-10"
+          className="absolute z-10"//太陽より後ろ
           style={{
             top: cloud.top,
             animation: `slide ${cloud.duration} linear infinite`,
