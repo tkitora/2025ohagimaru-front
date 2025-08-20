e371782e96b105f11cd3ad8c9e088afa176bae1f
yamanekt <yamakei0929neta@gmail.com>
2025-08-21 06:07:15 +0900

    未完成の図鑑とポップアップ

--- src/components/flowerDetails.tsx
+++ src/components/flowerDetails.tsx
@@ -10,10 +10,10 @@ const images = import.meta.glob("../assets/flowerImages/*.png", {
 //花の詳細ポップアップ
 const FlowerPopup = ({ flower, onClose }: { flower: FlowerList, onClose:() => void }) => {
     return (
-        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-opacity-75 flex items-center justify-center z-50">
      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
         <h2 className="text-2xl font-bold mb-4">{flower.name}</h2>
-        <p>種類: {flower.flowertype}</p>
+        <p>花の名前: {flower.flowertype}</p>
         <button onClick={onClose} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
           閉じる
         </button>
